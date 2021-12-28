4c161aea2f810c2f1b8949b4e10b7e45c410e42b
Keiren-b <keiren_b@hotmail.com>
2021-12-28 16:17:47 +0100

    display now updates with result

--- java.js
+++ java.js
@@ -1,54 +1,3 @@
-function add(num1,num2){
-    let result = num1+num2
-    alert(result)
-    return result;
-    
-}
-
-function subtract(num1,num2){
-    let result = num1-num2
-    alert(result)
-    return result
-}
-
-function multiply(num1,num2){
-    let result = num1*num2
-    alert(result)
-    return result
-}
-
-function divide(num1,num2){
-    let result = num1/num2
-    alert(result)
-    return result
-}
-
-function operate(operator, num1, num2){
-
-    if (operator === '+'){
-        let result = add(num1,num2)
-        return result
-    }
-
-    else if (operator === '-'){
-        let result = subtract(num1,num2)
-        return result
-    }
-
-    else if (operator === '*'){
-        let result = multiply(num1,num2)
-        return result
-    }
-
-    else if (operator === '/'){
-        let result = divide(num1,num2)
-        return result
-    }
-
-}
-
-//button creation
-
 let calcBtns = [1,2,3,4,5,6,7,8,9,0, ' + ', ' - ',' * ',' / ',' = ','Clear']
 let display = document.getElementById('display');
 let totalDisplay = ''
@@ -56,6 +5,50 @@ let testNum = 0
 let num1 = 0
 let num2 = 0
 let operator = ''
+let result = 0
+
+function add(num1,num2){
+    return num1+num2
+}
+
+function subtract(num1,num2){
+    return num1-num2
+    
+}
+
+function multiply(num1,num2){
+    return num1*num2
+}
+
+function divide(num1,num2){
+    return num1/num2
+}
+
+function operate(operator, num1, num2){
+
+    if (operator === '+'){
+        result = add(num1,num2)
+        return result
+    }
+
+    else if (operator === '-'){
+        result = subtract(num1,num2)
+        return result
+    }
+
+    else if (operator === '*'){
+        result = multiply(num1,num2)
+        return result
+    }
+
+    else if (operator === '/'){
+        result = divide(num1,num2)
+        return result
+    }
+}
+
+//button creation
+
 
 function createBtn(calcBtns){
 
@@ -103,15 +96,16 @@ function test(){
         findOperator()
         num1=parseInt(totalDisplay.slice(0, totalDisplay.indexOf(operator)))
         num2=parseInt(totalDisplay.slice(totalDisplay.indexOf(operator)+2))
-        
-
-      
         operate(operator, num1, num2)
-    
+        alert(result)
+        updateTextContent()
     })}
 
 test()
 
+function updateTextContent(){
+    display.textContent = result
+}
 
 
 
