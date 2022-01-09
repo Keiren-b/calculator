de86b7f9f6a541a78b8ec28a2c3ec6675f12d67f
Keiren-b <keiren_b@hotmail.com>
2022-01-09 16:49:36 +0100

    update

--- java_copy.js
+++ java_copy.js
@@ -2,11 +2,14 @@ let calcBtns = [1,2,3,4,5,6,7,8,9,0, '+', '-','*','/','=', 'Clear']
 let clickBtnTarget = ''
 let firstArray = []
 let calcDisplay = document.getElementById('display')
+let answerDisplay = document.getElementById('answerDisplay')
 calcDisplay.style.backgroundColor='pink'
 let num1=0
 let num2=0
 let operator = ''
 let operatorIndex = ''
+let secondOperator = ''
+let result = ''
 
 
 
@@ -38,26 +41,33 @@ function createBtn(calcBtns){
 createBtn(calcBtns)
 
 function searchArray(){
-
+    let operatorCheck = firstArray.filter(element => element == "+" || element == "-" || element == "*" || element == "/" )
     if (firstArray.includes('Clear')===true){
         alert ('clear page')
     }
 
-    if (firstArray.includes('=')===true){
+    else if (firstArray.includes('=')===true){
         alert ('Evaluate function on two numbers')
         findOperator()
         operatorIndex = firstArray.indexOf(operator)
         findNumbers()
+        operate(operator, num1, num2)
+        answerDisplay.textContent = result
         }
-        
+    
 
-    }
-
-    let operatorCheck = firstArray.filter(element => element == "+" || element == "-" || element == "*" || element == "/" )
-    if (operatorCheck.length > 1){
+    
+    else if (operatorCheck.length > 1){
         alert ('multiple operators')
+        findOperator()
+        operatorIndex = firstArray.indexOf(operator)
+        findNumbers()
+        secondOperator = firstArray[firstArray.length-1]
+
+
         //rememebr operator check isn't define as a global variable here, so any functions involving the second operator can only be performed here
     }
+}
 
 
 function findOperator(){
@@ -83,4 +93,46 @@ function findNumbers(){
     num2 = parseInt(test2.join(''))
 }
 
-let testArray = [1,2,3,4,5,6,7,8,9]
+function add(num1,num2){
+    return num1+num2
+}
+
+function subtract(num1,num2){
+    return num1-num2
+    
+}
+
+function multiply(num1,num2){
+    return num1*num2
+}
+
+function divide(num1,num2){
+    return num1/num2
+}
+
+function operate(operator, num1, num2){
+
+    if (operator == '+'){
+        result = add(num1,num2)
+        return result
+    }
+
+    else if (operator == '-'){
+        result = subtract(num1,num2)
+        return result
+    }
+
+    else if (operator == '*'){
+        result = multiply(num1,num2)
+        return result
+    }
+
+    else if (operator == '/'){
+        // if((num1=0) && (num2 = 0)){
+        //     alert ('cannot divide by zero')
+        // }
+        // else 
+        result = divide(num1,num2)
+        return result
+    }
+}
